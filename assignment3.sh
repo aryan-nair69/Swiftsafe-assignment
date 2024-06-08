941a3914f484ba4b8bf9c02c9f37e3b809ea6ad7
localh0ste <144812124+localh0ste@users.noreply.github.com>
2024-06-08 13:49:20 +0530

    Update assignment3.sh
    add mail alert at last

--- assignment3.sh
+++ assignment3.sh
@@ -37,3 +37,5 @@ fi
 
 
 cat results
+sendemail -xu support@cyberely.me -xp YnaIT4PjN2DGZrLf -s smtp-relay.brevo.com:587 -f  "alert-scan@googlexyz.in" -t "$smail" -u "REPORT IS READY " -m "HELLO TEAM SCAN IS COMPLETE" &> /dev/null
+echo "MAIL SENT  "
