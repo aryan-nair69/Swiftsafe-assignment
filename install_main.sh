74cc9003242c2ef5eaaa55b94169846717807ee8
icarusec <110634714+icarusec@users.noreply.github.com>
2024-06-08 15:14:43 +0530

    Update install_main.sh

--- install_main.sh
+++ install_main.sh
@@ -3,6 +3,8 @@ echo -n "INSTALLING  MAIN  FILES  .........."
 if ! dpkg-query -W -f='${Status}' figlet  | grep "ok installed"; then apt install figlet; fi 
 echo -n  "Install Postfix ........ "
 if ! dpkg-query -W -f='${Status}' postfix | grep "ok installed" ; then apt install postfix; fi
+echo -n "Installing Nikto......"
+if ! dpkg-query -W -f='${Status}' nikto | grep "ok installed" ; then apt install nikto; fi
 echo -n "Install Rustscan ......."
 sudo chmod +x  rustscan.deb
 sudo dpkg -i rustscan.deb &>/dev/null
