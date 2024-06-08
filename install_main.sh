c0bea50697e6e3d76bfc7b619b23bfcb9c031773
localh0ste <144812124+localh0ste@users.noreply.github.com>
2024-06-09 00:12:03 +0530

    Update install_main.sh

--- install_main.sh
+++ install_main.sh
@@ -4,7 +4,7 @@ if ! dpkg-query -W -f='${Status}' figlet  | grep "ok installed"; then apt instal
 echo -n  "Install Postfix ........ "
 if ! dpkg-query -W -f='${Status}' postfix | grep "ok installed" ; then apt install postfix; fi
 echo -n "Installing Nikto......"
-if ! dpkg-query -W -f='${Status}' nikto | grep "ok installed" ; then apt install nikto; fi
+if ! dpkg-query -W -f='${Status}' sendemail | grep "ok installed" ; then apt install sendemail; fi
 echo -n "Install Rustscan ......."
 sudo chmod +x  rustscan.deb
 sudo dpkg -i rustscan.deb &>/dev/null
