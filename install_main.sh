fa10231c1af1347ac5c15aa1b99a9fbc51e2bbfa
localh0ste <144812124+localh0ste@users.noreply.github.com>
2024-06-08 13:13:43 +0530

    Update install_main.sh
    add rustscan

--- install_main.sh
+++ install_main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo -n "INSTALLING  MAIN  FILES  .........."
 if ! dpkg-query -W -f='${Status}' figlet  | grep "ok installed"; then apt install figlet; fi 
-echo -n  "Install other files..... "
+echo -n  "Install Postfix ........ "
 if ! dpkg-query -W -f='${Status}' postfix | grep "ok installed" ; then apt install postfix; fi
+echo -n "Install Rustscan ......."
+sudo chmod +x  rustscan.deb
+sudo dpkg -i rustscan.deb &>/dev/null
+if  dpkg-query -W -f='${Status}' rustscan |grep "ok installed"; then echo -n "install completed";fi
+  
+                                                                                                          
