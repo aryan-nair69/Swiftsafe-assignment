1acd65bbdfa4c077a9cf9035c0012e9b6b86a510
aryan-nair69 <114796890+aryan-nair69@users.noreply.github.com>
2024-06-08 03:29:15 -0400

    Add files via upload

---
+++ assignment3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+        echo "Usage: ./assignment3.sh <IP>"
+        exit 1
+fi
+
+printf "\n----- Rustscan -----\n\n" > results
+
+echo "Running Rustscan..."
+#add your rustscan command >> results
+
+while read line
+do
+        if [[ $line == *open* ]] && [[ $line == *http* ]]
+        then
+                echo "Running Feroxbuster..."
+                #add feroxbuster command > temp1
+		#you can also add a variable to specify the word list
+    
+        
+done < results
+
+if [ -e temp1 ]
+then
+        printf "\n----- DIRS -----\n\n" >> results
+        cat temp1 >> results
+        rm temp1
+fi
+
+
+
+cat results
