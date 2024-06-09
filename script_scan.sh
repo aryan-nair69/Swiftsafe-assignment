dacd1da940d9f0985c005ecd3d66b6154c0e0083
aryan-nair69 <114796890+aryan-nair69@users.noreply.github.com>
2024-06-09 08:58:43 -0400

    Update script_scan.sh

--- script_scan.sh
+++ script_scan.sh
@@ -70,7 +70,9 @@ fi
 # Display results
 echo -e "Result is saved in Result.txt"
 while [[ ! "$smail" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; do
-  echo "Please enter a valid email address."
+  echo "Please enter a valid email
+ address."
+  exit 1;
 done
 
 # Send email with the results
