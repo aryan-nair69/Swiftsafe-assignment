fe8d14e6e76b59d5b65a9aade26cc2359d2b455c
localh0ste <144812124+localh0ste@users.noreply.github.com>
2024-06-09 13:11:25 +0530

    Update install_main.sh

--- install_main.sh
+++ install_main.sh
@@ -3,7 +3,7 @@ echo -n "INSTALLING  MAIN  FILES  .........."
 if ! dpkg-query -W -f='${Status}' figlet  | grep "ok installed"; then apt install figlet; fi 
 echo -n  "Install Postfix ........ "
 if ! dpkg-query -W -f='${Status}' postfix | grep "ok installed" ; then apt install postfix; fi
-echo -n "Installing Nikto......"
+echo -n "Installing sendemail......"
 if ! dpkg-query -W -f='${Status}' sendemail | grep "ok installed" ; then apt install -y sendemail; fi
 echo -n "Install Rustscan ......."
 sudo chmod +x  rustscan.deb
@@ -13,7 +13,9 @@ echo -n "Install Feroxbuster it will take some time ........"
 if ! dpkg-query -W -f='${Status}' feroxbuster | grep "ok installed";then sudo apt install -y feroxbuster &> /dev/null ;fi
 echo -e  "Thank you , Everything is installed Now , go to run.sh \n\n"
 chmod 777 feroxbuster
-figlet  DONE
+GREEN='\033[0;32m'
+echo -e "${GREEN} We install Everything ! Happy day Team 1 \n"
+figlet  Install_Done
 echo -e  "\\033[31mTEAM 1 PROJECT SWIFT SAFE \\033[0m"
 sleep 2s
 
